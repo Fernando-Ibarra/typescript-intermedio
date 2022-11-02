e0398c81b34d592252315864c0889747dfd1c406
Fernando-Ibarra <fi94457@gmail.com>
2022-11-02 09:11:11 -0600

    [02-tuples]

---
+++ src/02-tuples.ts
@@ -0,0 +1,10 @@
+// array
+const prices: (number | string)[] = [1, 2, 3, 5, 'as']
+
+// tupla
+const user: [string, number, boolean] = ['juancalvo', 15, true]
+
+// destructuracion
+const [username, age] = user;
+console.log(username);
+console.log(age);
