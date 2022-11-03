e7b9cd6c1fea2f7ccc2d79cf04326904ddfe1e26
Fernando-Ibarra <fi94457@gmail.com>
2022-11-02 20:34:58 -0600

    [extendsInterfaces]

--- src/App/main.ts
+++ src/App/main.ts
@@ -2,11 +2,14 @@ import { addProduct } from './products/product.service'
 
 addProduct({
   id: '1',
-  title: 'Producto 1',
   createdAt: new Date(),
+  updatedAt: new Date(),
+  title: 'Producto 1',
   stock:90,
   category: {
     id: '12',
-    name: 'category 1'
+    name: 'category 1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }
 })
--- src/App/orders/order.model.ts
+++ src/App/orders/order.model.ts
@@ -1,10 +1,8 @@
+import { BaseModel } from '../base.model'
 import { Product } from '../products/product.model'
 import { User } from '../users/user.model'
 
-
-interface Order {
-  id: string | number;
-  createdAt: Date;
-  product: Product[];
+interface Order extends BaseModel {
+  product: Product[]
   user: User;
 }
